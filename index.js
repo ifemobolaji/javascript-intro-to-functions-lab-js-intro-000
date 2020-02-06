c7a945e7ae7556cb5e140fc4983c2481ebc519f6
loyal-transpiler-0513 <ifeawoyale@yahoo.com>
2020-02-06 23:53:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
+function logShout(string) {
+  console.log();
+}
+
+function logWhisper(string) {
+  console.log();
+}
